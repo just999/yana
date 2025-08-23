e05a53d30a044aad4228978d5eb42c6d03266411
tommy88888 <tommydewa08@gmail.com>
2025-08-23 23:07:38 +0700

    fix const 4

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -13,24 +13,23 @@ export const SITE_CONFIG = {
 };
 
 export const loginDefaultValues = {
-  email: 'user_sembilan@gmail.com',
-  password: '111111',
+  email: '',
+  password: '',
   remember: false,
   code: '',
 };
 export const registerDefaultValues = {
-  name: 'user sembilan',
-  email: 'user_sembilan@gmail.com',
-  password: '111111',
-  confirmPassword: '111111',
+  name: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
   anonymous: false,
   avatar: '',
-  phone: '0821-6888-9060',
-  school: 'Universitas Sumatera Utara ',
-  city: 'Medan',
-  address:
-    'Jalan Dr. T. Mansur No.9, Padang Bulan, Kec. Medan Baru, Kota Medan, Sumatera Utara 20222',
-  major: 'S1 - Kesehatan ',
+  phone: '',
+  school: '',
+  city: '',
+  address: '',
+  major: '',
   terms: false,
 };
 
